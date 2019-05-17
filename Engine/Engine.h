5bf6e874f6478ea1b6abbac09df9dcd13117391d
Kang Il Son <opengl83@gmail.com>
2019-05-17 18:08:13 +0900

    UPDATE

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -1,1 +1,8 @@
 
+#   ifdef ENGINE_EXPORTS
+#       define ENGINE_API __declspec(dllexport)
+#   elif defined(ENGINE_IMPORTS)
+#       define ENGINE_API __declspec(dllimport)
+#   else
+#       define ENGINE_API
+#   endif
