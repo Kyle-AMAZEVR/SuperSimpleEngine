892822f7e7a852a08334f49e6481faa7dba22969
Kang Il Son <opengl83@gmail.com>
2019-05-21 18:34:22 +0900

    Update DXShader.h

--- Engine/DXShader.h
+++ Engine/DXShader.h
@@ -1,15 +1,21 @@
 #pragma once
 
+
 class ENGINE_API DXShader
 {
 public:
-
+    virtual ~DXShader() {}
+protected:
+    
 };
 
 class ENGINE_API DXVertexShader : public DXShader
 {
 public:
-
+    DXVertexShader(){}
+    DXVertexShader(const std::wstring filename);
+    virtual ~DXVertexShader();    
 protected:
-    ID3D11VertexShader* mVertexShader;
+    ID3D11VertexShader* mVertexShader = nullptr;
 };
+
