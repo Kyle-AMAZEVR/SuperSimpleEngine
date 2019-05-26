82ce9e3e2acde085a69409f62b53cb1ce27a2409
Kang Il Son <opengl83@gmail.com>
2019-05-26 16:54:23 +0900

    update

--- Engine/DXIndexBuffer.cpp
+++ Engine/DXIndexBuffer.cpp
@@ -1,3 +1,22 @@
 #include "Core.h"
+#include "DXEngine.h"
 #include "DXIndexBuffer.h"
 
+
+void DXIndexBuffer::SetIndexBufferData(const std::vector<unsigned int> indexData)
+{
+    mBufferDescription.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    mBufferDescription.CPUAccessFlags = 0;
+    mBufferDescription.Usage = D3D11_USAGE_DYNAMIC;
+    mBufferDescription.MiscFlags = 0;
+    mBufferDescription.StructureByteStride = 0;
+    mBufferDescription.ByteWidth = sizeof(unsigned int) * indexData.size();
+
+    D3D11_SUBRESOURCE_DATA indexSubresourceData;
+
+    indexSubresourceData.pSysMem = &indexData[0];
+	indexSubresourceData.SysMemPitch = 0;
+	indexSubresourceData.SysMemSlicePitch = 0;
+
+    HR(DXEngine::Get().GetDevice()->CreateBuffer(&mBufferDescription, &indexSubresourceData, &mpBuffer));
+}
--- Engine/DXIndexBuffer.h
+++ Engine/DXIndexBuffer.h
@@ -1,9 +1,15 @@
 
 #pragma once
 
+#include <vector>
 #include "DXBufferBase.h"
 
 class DXIndexBuffer : public DXBufferBase
 {
 public:
+    DXIndexBuffer();
+
+    void SetIndexBufferData(const std::vector<unsigned int> indexData);
+protected:
+    
 };
