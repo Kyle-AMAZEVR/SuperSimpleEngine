3f3d706c2224a9ae82283e2ef0fb29662d10807b
Kang Il Son <opengl83@gmail.com>
2019-07-29 15:13:07 +0900

    UDPATE

--- Engine/SSRenderTarget2D.cpp
+++ Engine/SSRenderTarget2D.cpp
@@ -9,40 +9,51 @@ SSRenderTarget2D::SSRenderTarget2D(const UINT width, const UINT height, DXGI_FOR
 }
 void SSRenderTarget2D::InternalCreate(const UINT width, const UINT height, DXGI_FORMAT format)
 {
-	D3D11_TEXTURE2D_DESC description;
-	description.Width = mWidth = width;
-	description.Height = mHeight = height;
-	description.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
-	description.MiscFlags = 0;
-	description.Usage = D3D11_USAGE_DEFAULT;
-	description.SampleDesc.Count = 1;
-	description.MipLevels = description.ArraySize = 1;
-	description.CPUAccessFlags = 0;
-	description.Format = mTextureFormat = format;
+	D3D11_TEXTURE2D_DESC textureDesc{ 0 };
+	textureDesc.Width = mWidth = width;
+	textureDesc.Height = mHeight = height;
+	textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+	textureDesc.MiscFlags = 0;
+	textureDesc.Usage = D3D11_USAGE_DEFAULT;
+	textureDesc.SampleDesc.Count = 1;
+	textureDesc.MipLevels = textureDesc.ArraySize = 1;
+	textureDesc.CPUAccessFlags = 0;
+	textureDesc.Format = mTextureFormat = format;
 
-	HR(SSEngine::Get().GetDevice()->CreateTexture2D(&description, nullptr, &mTexturePtr));
-	//HR(DXEngine::Get().GetDevice()->CreateShaderResourceView(mTexturePtr, nullptr, &mShaderResourceView));
+	HR(SSEngine::Get().GetDevice()->CreateTexture2D(&textureDesc, nullptr, &mTexturePtr));
+
+	D3D11_RENDER_TARGET_VIEW_DESC renderTargetDesc;
+	ZeroMemory(&renderTargetDesc, sizeof(D3D11_RENDER_TARGET_VIEW_DESC));
+
+	renderTargetDesc.Format = textureDesc.Format;
+	renderTargetDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	renderTargetDesc.Texture2D.MipSlice = 0;
+
+	HR(SSEngine::Get().GetDevice()->CreateRenderTargetView(mTexturePtr, &renderTargetDesc, &mRenderTargetView));
+
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+	ZeroMemory(&shaderResourceViewDesc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
+	
+	shaderResourceViewDesc.Format = textureDesc.Format;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+
+	HR(SSEngine::Get().GetDevice()->CreateShaderResourceView(mTexturePtr, &shaderResourceViewDesc, &mShaderResourceView));
 }
 
-void SSRenderTarget2D::Release()
+void SSRenderTarget2D::Destroy()
 {
-	if (mTexturePtr != nullptr)
-	{
-		mTexturePtr->Release();
-		mTexturePtr = nullptr;
-	}
-	if (mShaderResourceView != nullptr)
-	{
-		mShaderResourceView->Release();
-		mShaderResourceView = nullptr;
-	}
+	ReleaseCOM(mTexturePtr);
+	ReleaseCOM(mShaderResourceView);
+	ReleaseCOM(mRenderTargetView);
 }
 
 void SSRenderTarget2D::Resize(const UINT newWidth, const UINT newHeight)
 {
 	if (mWidth != newWidth || mHeight != newHeight)
 	{
-		Release();
+		Destroy();
 		InternalCreate(newWidth, newHeight, mTextureFormat);
 	}
 }
--- Engine/SSRenderTarget2D.h
+++ Engine/SSRenderTarget2D.h
@@ -7,12 +7,10 @@ class ENGINE_API SSRenderTarget2D : public SSTexture2DBase
 public:
 	SSRenderTarget2D(const UINT width, const UINT height, DXGI_FORMAT eFormat);
 	virtual void Resize(const UINT newWidth, const UINT newHeight) override;
-	void Release();
+	virtual void Destroy() override;
 protected:
 	ID3D11RenderTargetView* mRenderTargetView = nullptr;
 	ID3D11ShaderResourceView* mShaderResourceView = nullptr;
-	ID3D11DepthStencilView* mDepthStencilView = nullptr;
-
 	void InternalCreate(const UINT newWidth, const UINT height, DXGI_FORMAT format);
 	
 };
