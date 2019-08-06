bc20bc984e81caa8fcaf8f5dd8586e0e47c2d68d
Kang Il Son <opengl83@gmail.com>
2019-08-06 14:23:32 +0900

    update

--- Engine/SSName.cpp
+++ Engine/SSName.cpp
@@ -2,7 +2,54 @@
 #include "Core.h"
 #include "SSName.h"
 
-SSName::SSName(std::string name)
+SSName::SSName(const std::string& name)
 {
 	mHashValue = std::hash<std::string>{} {name};
+	mBucketIndex = SSNameBucket::Get().AddName(mHashValue, name);
+}
+
+std::string SSName::ToString() const 
+{
+	return SSNameBucket::Get().GetName(this);
+}
+
+//
+
+size_t SSNameBucket::AddName(size_t hashValue, const std::string& name)
+{
+	if (mNameBucket.count(hashValue) > 0)
+	{
+		auto& bucket = mNameBucket[hashValue];
+		
+		for (size_t i = 0; i < bucket.size(); ++i)
+		{
+			if (bucket[i] == name)
+			{
+				return i;
+			}
+		}
+
+		bucket.push_back(name);
+		return bucket.size() - 1;
+	}
+	else
+	{
+		std::vector<std::string> temp;
+		mNameBucket[hashValue] = temp;
+		mNameBucket[hashValue].push_back(name);
+		return 0;
+	}	
+}
+
+std::string SSNameBucket::GetName(const SSName* name) const
+{
+	if (mNameBucket.count(name->mHashValue) > 0)
+	{
+		const auto& v = mNameBucket.at(name->mHashValue);
+		return v[name->mBucketIndex];
+	}
+	else
+	{
+		return "";
+	}
 }
--- Engine/SSName.h
+++ Engine/SSName.h
@@ -2,19 +2,74 @@
 
 #pragma once
 
+#include <map>
 #include <unordered_map>
+#include <vector>
+#include "Singleton.h"
 
 class ENGINE_API SSName
 {
 public:
-	SSName(std::string name);
-	SSName(std::wstring name);
+	SSName(const std::string& name);	
 
-	
+	bool operator < (const SSName& rhs)
+	{
+		if (mHashValue < rhs.mHashValue)
+		{
+			return true;
+		}
+
+		if (mBucketIndex < rhs.mBucketIndex)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	bool operator > (const SSName& rhs)
+	{	
+		if (mHashValue > rhs.mHashValue)
+		{
+			return true;
+		}
+
+		if (mBucketIndex > rhs.mBucketIndex)
+		{
+			return true;
+		}		
+
+		return false;
+	}
+
+	bool operator == (const SSName& rhs)
+	{
+		return mHashValue == mHashValue && mBucketIndex == rhs.mBucketIndex;
+	}
+
+	bool operator != (const SSName& rhs)
+	{
+		return !(*this == rhs);
+	}
+
+	std::string ToString() const;
+
+protected:
+	friend class SSNameBucket;
+	size_t mHashValue = 0;
+	size_t mBucketIndex = 0;
+};
+
+
+class ENGINE_API SSNameBucket : public Singleton<SSNameBucket>
+{
+public:
+	size_t AddName(size_t hashValue, const std::string& name);
+	std::string GetName(const SSName* name) const;
 
 protected:
 
-	size_t mHashValue = 0;
-};
+	std::map<size_t, std::vector<std::string>> mNameBucket;
 
+};
 
