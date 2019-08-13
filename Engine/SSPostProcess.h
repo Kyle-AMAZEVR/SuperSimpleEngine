209b8dc5d65070f699e18b24e3b3c3f0260fcd3c
Kang Il Son <opengl83@gmail.com>
2019-08-13 18:39:56 +0900

    Update SSPostProcess.h

--- Engine/SSPostProcess.h
+++ Engine/SSPostProcess.h
@@ -1,15 +1,31 @@
 #pragma once
 
 
-class ENGINE_API SSPostProcess
+class ENGINE_API SSPostProcess : public DXRenderResource
 {
 public:	
-	SSPostProcess(UINT);
+	SSPostProcess(UINT width , UINT height, UINT rtCount=1, bool bDepthExist =false, bool bFixed =false);
 	virtual ~SSPostProcess();
 
 	virtual void Draw(SSTexture2DBase* input0) {}
 	virtual void Draw(SSTexture2DBase* input0, SSTexture2DBase* input1) {}
+	virtual void OnResize(UINT width, UINT height){}
+
+	UINT GetWidth() const { return mWidth; }
+	UINT GetHeight() const { return mHeight; }
 
 protected:
-	class SSRenderTargetTexture2D* mOutput;
+	std::unique_ptr<class SSGenericRenderTarget> mRenderTarget;
+	UINT mWidth;
+	UINT mHeight;
+	bool mFixedSize = false;
+	bool mDepthExist = false;
+	UINT mRTCount = 1;
+};
+
+class ENGINE_API SSFXAAPostProcess : public SSPostProcess
+{
+public:
+
+	virtual void Destroy() override;
 };
