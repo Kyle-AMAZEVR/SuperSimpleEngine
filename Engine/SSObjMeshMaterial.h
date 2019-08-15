0c16fca932350bbb9c18bb8a31cd8fb261a3b32c
Kang Il Son <opengl83@gmail.com>
2019-08-15 20:05:02 +0900

    added texture

---
+++ Engine/SSObjMeshMaterial.h
@@ -0,0 +1,23 @@
+
+
+#pragma once
+
+
+#include <string>
+
+class SSObjMeshMaterial
+{
+public:
+		
+	std::string MaterialName;
+	
+	std::string DiffuseMap;
+	
+	std::string NormalMap;
+	
+	std::string SpecularMap;
+	
+	std::string RoughnessMap;
+	
+	std::string MaskMap;	
+};
