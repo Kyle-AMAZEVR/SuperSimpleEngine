28098920a42400b13b2df07571ddfbfe8a3bda58
Kang Il Son <opengl83@gmail.com>
2019-08-17 15:28:46 +0900

    update

--- Engine/SSGBufferDumpPostProcess.cpp
+++ Engine/SSGBufferDumpPostProcess.cpp
@@ -13,6 +13,8 @@ SSGBufferDumpPostProcess::SSGBufferDumpPostProcess(UINT width, UINT height)
 {
 	mVertexShader = SSShaderManager::Get().GetVertexShader("GBufferDump.vs");
 	mPixelShader = SSShaderManager::Get().GetPixelShader("GBufferDump.ps");
+
+	UpdateDumpSettings();
 }
 
 void SSGBufferDumpPostProcess::Draw(SSTexture2DBase* input0, SSTexture2DBase* input1, SSTexture2DBase* input2)
@@ -23,7 +25,43 @@ void SSGBufferDumpPostProcess::Draw(SSTexture2DBase* input0, SSTexture2DBase* in
 	SSDrawCommand gbufferDumpDrawCmd{ mVertexShader.get(), mPixelShader.get(), mScreenBlit };
 	gbufferDumpDrawCmd.SetPSTexture("PositionTex", input0);
 	gbufferDumpDrawCmd.SetPSTexture("DiffuseTex", input1);
-	gbufferDumpDrawCmd.SetPSTexture("NormalTex", input2);	
+	gbufferDumpDrawCmd.SetPSTexture("NormalTex", input2);
+
+	gbufferDumpDrawCmd.StorePSConstantBufferData("Dump", mDumpSettings);
 
 	gbufferDumpDrawCmd.Do();
+}
+
+void SSGBufferDumpPostProcess::ChangeNextDumpMode()
+{
+	switch(mDumpMode)
+	{
+	case EDumpMode::EPositionDump:
+		mDumpMode = EDumpMode::ENormalDump;
+		break;
+	case EDumpMode::ENormalDump:
+		mDumpMode = EDumpMode::EMetalicDump;
+		break;
+	case EDumpMode::EMetalicDump:
+		mDumpMode = EDumpMode::EDiffuseDump;
+		break;
+	case EDumpMode::EDiffuseDump:
+		mDumpMode = EDumpMode::ERoughnessDump;
+		break;
+	case EDumpMode::ERoughnessDump;
+		mDumpMode = EDumpMode::EPositionDump;
+		break;
+	}
+
+	UpdateDumpSettings();
+}
+ 
+
+void SSGBufferDumpPostProcess::UpdateDumpSettings()
+{
+	mDumpSettings.value1 = (mDumpMode == EDumpMode::EPositionDump);
+	mDumpSettings.value2 = (mDumpMode == EDumpMode::ENormalDump);
+	mDumpSettings.value3 = (mDumpMode == EDumpMode::EMetalicDump);
+	mDumpSettings.value4 = (mDumpMode == EDumpMode::EDiffuseDump);
+	mDumpSettings.value5 = (mDumpMode == EDumpMode::ERoughnessDump);
 }
--- Engine/SSGBufferDumpPostProcess.h
+++ Engine/SSGBufferDumpPostProcess.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "SSPostProcess.h"
-
+#include "FreqUsedConstantBufferTypes.h"
 #include "SSShader.h"
 
 
 enum class EDumpMode : unsigned char
 {
-	
+	EPositionDump,
+	ENormalDump,
+	EMetalicDump,
+	EDiffuseDump,
+	ERoughnessDump,
 };
 
 class SSGBufferDumpPostProcess : public SSPostProcess
@@ -16,9 +20,17 @@ public:
  
 	virtual void Draw(SSTexture2DBase* input0, SSTexture2DBase* input1, SSTexture2DBase* input2) override;
 
-	
+	void ChangeNextDumpMode();
 
 protected:
 	std::shared_ptr<SSVertexShader> mVertexShader;
 	std::shared_ptr<SSPixelShader> mPixelShader;
+
+	EDumpMode mDumpMode = EDumpMode::ENormalDump;
+
+	void UpdateDumpSettings();
+
+	SSAlignedCBuffer<int, int, int, int, int> mDumpSettings;
+
+
 };
