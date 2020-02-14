2ac62d1876d01405c5e47dd8b23dc267bbc508b9
Kang Il Son <opengl83@gmail.com>
2020-02-14 17:37:11 +0900

    dx 12

--- Engine/SSVertexElementDeclaration.h
+++ Engine/SSVertexElementDeclaration.h
@@ -13,6 +13,10 @@ public:
     // POSITION, NORMAL, TEXCOORD
     static D3D11_INPUT_ELEMENT_DESC PositionNormalTexcoord[3];
     
-	//
+	// POSITION, COLOR
+    static D3D12_INPUT_ELEMENT_DESC PositionColorElementDesc[2];
+
+    // POSITION, NORMAL, TEXCOORD
+    static D3D12_INPUT_ELEMENT_DESC PositionNormalTexcoordElementDesc[3];
 };
 
