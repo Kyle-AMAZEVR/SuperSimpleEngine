382c51898e76727ad39ce681c6afc1b4c295952f
Kang Il Son <opengl83@gmail.com>
2020-03-03 17:40:13 +0900

    shader compile

---
+++ Engine12/SSDX12Shader.cpp
@@ -0,0 +1,18 @@
+
+#include "SSDX12.h"
+#include "SSDX12Shader.h"
+#include <filesystem>
+
+
+
+void SSVertexShader::CompileFromFile(ID3D12Device* device, std::wstring filepath)
+{
+	D3D_SHADER_MACRO macro;
+
+	
+	ID3DBlob* errorMsg = nullptr;
+
+
+
+	
+}
---
+++ Engine12/SSDX12Shader.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+class SSDX12ShaderCompileOption
+{
+public:
+	SSDX12ShaderCompileOption();
+protected:
+	std::string mEntryPoint;
+	std::string mTargetName;
+	D3D_SHADER_MACRO mMacro;
+};
+
+
+
+class SSDX12Shader
+{
+public:
+	ID3DBlob* GetCompiledShader();
+
+protected:
+	virtual void ReflectCompiledShader(ID3D12ShaderReflection* reflection);
+	virtual void CompileFromFile(ID3D12Device* device, std::wstring filepath);	
+
+	ComPtr<ID3DBlob> mShaderBuffer = nullptr;
+};
+
+class SSVertexShader : public SSDX12Shader
+{
+public:
+	virtual void CompileFromFile(ID3D12Device* device, std::wstring filepath) override;
+
+};
