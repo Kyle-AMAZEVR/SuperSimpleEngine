44a96dbbe9a31e4553f2ad68c68ddb02c093eac9
Kang Il Son <opengl83@gmail.com>
2020-08-14 02:41:42 +0900

    Update DXApp.cpp

--- DXApp.cpp
+++ DXApp.cpp
@@ -33,9 +33,6 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
-HWND WindowHandle;
-SSRenderingThread renderingThread;
-
 
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -68,149 +65,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DXENGINE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DXENGINE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
 
 //
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Store instance handle in our global variable	
-
-   HWND hWnd = CreateWindowW(szWindowClass, L"SuperSimple", WS_OVERLAPPEDWINDOW,
-      0, 0, 480, 360, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);   
-   WindowHandle = hWnd;
-   return TRUE;
-}
-
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
-{
-    static int width = 0;
-    static int height = 0;
-	
-
-    switch (message)
-    {
-    case WM_COMMAND:
-        { 
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-			HDC hdc;
-            hdc = BeginPaint(hWnd, &ps);
-
-			
-			//renderingThread.WaitForRenderingThread();
-
-            // TODO: Add any drawing code that uses hdc here...            
-            EndPaint(hWnd, &ps);
-        }
-        break;
-
-    case WM_SIZE:
-        {
-            width = static_cast<int>(LOWORD(lParam));
-            height = static_cast<int>(HIWORD(lParam));
-			if (renderingThread.IsRunning())
-			{
-				renderingThread.ExecuteInRenderingThread([]()
-				{
-					//SSEngine12::Get().OnWindowResize(width, height);
-					SSDX11Engine::Get().OnWindowResize(width, height);
-				});
-			}
-        }
-        break;
-
-	case WM_KEYDOWN:
-		SSInputManager::Get().OnKeyDown(wParam);
-		break;
-
-	case WM_MOUSEMOVE:		
-		SSInputManager::Get().OnMouseMove(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam)); 
-		break;
-
-	case WM_LBUTTONDOWN:		
-		SSInputManager::Get().OnMouseLBtnDown(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-		break;
-
-	case WM_LBUTTONUP:
-		SSInputManager::Get().OnMouseLBtnUp();
-		break;
-	
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
 // Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
